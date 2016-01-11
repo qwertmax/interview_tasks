738994290ffcd8dbde420f10f45e21adbd8e8f5a
Maxim Tishchenko <qwertmax@gmail.com>
2016-01-11 17:07:22 +0600

    error_handling

--- error_handling.go
+++ error_handling.go
@@ -14,13 +14,27 @@ func (e *SyntaxError) Error() string { return e.msg }
 
 func main() {
 
-	v1()
+	v1("README.md")
+	v2("README.md_ERROR")
+	// v3("README.md_ERROR")
 }
 
-func v1() {
-	_, err := os.Open("README.md")
+func v1(fileName string) {
+	_, err := os.Open(fileName)
 	if err != nil {
 		if serr, ok := err.(*os.PathError); ok {
+			fmt.Printf("%#v\n", serr)
+		} else {
+			fmt.Printf("default err\n%#v\n", err)
+		}
+	}
+
+}
+
+func v2(fileName string) {
+	_, err := os.Open(fileName)
+	if err != nil {
+		if serr, ok := err.(*os.SyscallError); ok {
 			fmt.Printf("%#v\n", serr)
 		} else {
 			fmt.Printf("default err\n%#v\n", err)
