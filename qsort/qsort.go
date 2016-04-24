e39422e6b1cf1f75157a9ae6224b6299d64fbbfb
Maxim Tishchenko <qwertmax@gmail.com>
2016-04-25 00:03:09 +0600

    custom sort VS native sort

---
+++ qsort/qsort.go
@@ -0,0 +1,100 @@
+package qsort
+
+import (
+	// "fmt"
+	"math/rand"
+	"sort"
+	// "time"
+)
+
+type Interface interface {
+	sort.Interface
+	// Partition returns slice[:i] and slice[i+1:]
+	// These should references the original memory
+	// since this does an in-place sort
+	Partition(i int) (left Interface, right Interface)
+}
+
+type IntSlice []int
+
+func (is IntSlice) Less(i, j int) bool {
+	return is[i] < is[j]
+}
+
+func (is IntSlice) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+func (is IntSlice) Len() int {
+	return len(is)
+}
+
+func (is IntSlice) Partition(i int) (left Interface, right Interface) {
+	return IntSlice(is[:i]), IntSlice(is[i+1:])
+}
+
+func Qsort(a Interface, prng *rand.Rand) Interface {
+	if a.Len() < 2 {
+		return a
+	}
+
+	left, right := 0, a.Len()-1
+
+	// Pick a pivot
+	pivotIndex := prng.Int() % a.Len()
+	// Move the pivot to the right
+	a.Swap(pivotIndex, right)
+
+	// Pile elements smaller than the pivot on the left
+	for i := 0; i < a.Len(); i++ {
+		if a.Less(i, right) {
+
+			a.Swap(i, left)
+			left++
+		}
+	}
+
+	// Place the pivot after the last smaller element
+	a.Swap(left, right)
+
+	// Go down the rabbit hole
+	leftSide, rightSide := a.Partition(left)
+	Qsort(leftSide, prng)
+	Qsort(rightSide, prng)
+
+	return a
+}
+
+func QuickSort(slice []int) []int {
+	length := len(slice)
+
+	if length <= 1 {
+		sliceCopy := make([]int, length)
+		copy(sliceCopy, slice)
+		return sliceCopy
+	}
+
+	m := slice[rand.Intn(length)]
+
+	less := make([]int, 0, length)
+	middle := make([]int, 0, length)
+	more := make([]int, 0, length)
+
+	for _, item := range slice {
+		switch {
+		case item < m:
+			less = append(less, item)
+		case item == m:
+			middle = append(middle, item)
+		case item > m:
+			more = append(more, item)
+		}
+	}
+
+	less, more = QuickSort(less), QuickSort(more)
+
+	less = append(less, middle...)
+	less = append(less, more...)
+
+	return less
+}
---
+++ qsort/qsort_test.go
@@ -0,0 +1,50 @@
+package qsort_test
+
+import (
+	"github.com/qwertmax/interview_tasks/qsort"
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+const size int = 1000000
+
+var list = make([]int, size)
+var prng = rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+
+func BenchmarkQsort(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		for i := range list {
+			list[i] = prng.Int()
+		}
+		b.StartTimer()
+
+		qsort.Qsort(qsort.IntSlice(list), prng)
+	}
+}
+
+func BenchmarkNativeQsort(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		for i := range list {
+			list[i] = prng.Int()
+		}
+		b.StartTimer()
+
+		qsort.NativeQsort(list, prng)
+	}
+}
+
+func BenchmarkSort(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		for i := range list {
+			list[i] = prng.Int()
+		}
+		b.StartTimer()
+
+		sort.Sort(sort.IntSlice(list))
+	}
+}
