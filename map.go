9614a77c393a21a3a3ff7a5c4f41792c9366a2de
Maxim Tishchenko <qwertmax@gmail.com>
2016-05-07 09:51:26 +0600

    useing map[]

--- map.go
+++ map.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
-func main() {
+type user struct {
+	name, email string
+}
 
+func (u *user) ChangeEmail(email string) {
+	u.email = email
+}
+
+type userGroup struct {
+	users map[int]*user
+}
+
+func main() {
+	ug := userGroup{user{name: "Max", email: "1@ex.com"}}
 }
