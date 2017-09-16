0722a2ad5735a2624bc96f2f181d1ad396835df8
Maxim Tishchenko <qwertmax@gmail.com>
2017-09-16 20:34:21 +0200

    Candy problem

---
+++ candy.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func candy(ratings []int) int {
+	if len(ratings) == 0 {
+		return 0
+	}
+	reduceLength, growLength := 0, 1
+
+	pre := 1
+	ret := 1
+	for i := 1; i < len(ratings); i++ {
+		if ratings[i] < ratings[i-1] {
+			reduceLength++
+			if reduceLength >= pre {
+				ret++
+			}
+			ret += reduceLength
+			growLength = 1
+		} else {
+			cur := 1
+			if ratings[i] > ratings[i-1] {
+				cur = growLength + 1
+			}
+			growLength = cur
+			ret += cur
+			reduceLength = 0
+			pre = cur
+		}
+	}
+	return ret
+}
+
+func main() {
+	fmt.Printf("%#v\n", candy([]int{1, 2, 3, 4, 5, 6, 7}))
+}
